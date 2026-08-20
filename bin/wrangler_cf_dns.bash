0f5c42c463590bf6943f005314d26920f5c9e83b
Spencer Butler <dev@tcos.us>
2026-08-20 04:35:46 -0500

    feat(ingress): migrate repo structure and implement native worker proxy bypass

--- bin/wrangler_cf_dns.bash
+++ bin/wrangler_cf_dns.bash
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 export CLOUDFLARE_ACCOUNT_ID="a33d047ae2835100b8ea875863913f96"
-export CLOUDFLARE_API_TOKEN="cfut_4MrkMUsI8gPcJYOtdyDBFUSA1MsEqwwjYU4ndKLLf303d575"
+export CLOUDFLARE_API_TOKEN=""
 
 npx wrangler pages domains add resume spencer.blog.tcos.us
 
