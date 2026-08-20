bb282953dbf667d2a51c2817783c31c392686063
Spencer Butler <dev@tcos.us>
2026-08-20 04:26:06 -0500

    chore(creds): new security is mean. moved wrangler* files to my homebin

--- bin/wrangler_cf_dns.bash
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-export CLOUDFLARE_ACCOUNT_ID="a33d047ae2835100b8ea875863913f96"
-export CLOUDFLARE_API_TOKEN=""
-
-npx wrangler pages domains add resume spencer.blog.tcos.us
-
